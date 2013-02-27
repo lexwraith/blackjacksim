b5fce9d134714fec2d2bb1885e67582b3cfad9af
LexWraith <lex.wraith@gmail.com>
2013-02-26 21:25:15 -0500

    Finished template.

--- blackjack.py
+++ blackjack.py
@@ -17,7 +17,7 @@ class decks:
 		self.shoe = []
 		self.generate()
 		self.garbage = []
-
+		self.count = 0
 	def generate(self):
 		self.shoe = [(self.deckstyle[i]) for i in range(13) for z in range(4) for j in range(self.size)]
 		for x in range(2): shuffle(self.shoe)
@@ -26,6 +26,7 @@ class decks:
 		if(len(self.shoe) == 0): 
 			self.shoe = self.garbage
 			self.garbage = []
+			self.count = 0
 		toreturn = self.shoe[0]
 		del self.shoe[0]
 		return toreturn;
@@ -36,15 +37,18 @@ class decks:
 	def showshoe(self):
 		print(self.shoe)
 
+	def showgarbage(self):
+		print(self.garbage)
+
 	def isempty(self):
 		if(len(self.shoe) == 0): return True
 
-	def get(self,card):
+	def get(self,hand):
 		"""
-		Used for leftover cards in the event the dealer
-		runs out of cards for the current hand and needs
-		to use old ones.
+		For collecting up used cards in garbage
 		"""
+		for elem in hand:
+			self.garbage.append(elem)
 
 class player:
 	"""Players that can come in and out"""
@@ -111,6 +115,7 @@ def runtests(numplayers,shoesize,magicnumber,bankroll,lowbet,highbet):
 	players = [player(bankroll,lowbet,highbet) for z in range(numplayers)]
 	table = player(1000000,5,25)
 	shoe = decks(shoesize,standard)
+	shoe.count = 0
 	rounds = 0
 	while(rounds < 1000):
 		#Counter
@@ -118,29 +123,69 @@ def runtests(numplayers,shoesize,magicnumber,bankroll,lowbet,highbet):
 
 		#Players placing their bets
 		for person in players:
-			person.bet = person.high if person.betbig else person.low
-			person.bank - person.bet
+			if(shoe.count > 0): 
+				person.bet = person.high
+			else: 
+				person.bet = person.low 
+			person.bank -= person.bet
+			#person.bet = person.high if person.betbig else person.low
+			#person.bank -= person.bet
 
 		#Dealing the cards
 		for z in range(2):
 			for x in range(len(players)):
-				players[x].get(shoe.spit())
-			table.get(shoe.spit())
+				mybuffer = shoe.spit()
+				if(mybuffer in [2,3,4,5,6,7]):
+					shoe.count += 1
+				if(mybuffer == 10):
+					shoe.count -= 2
+				if(mybuffer == "A"):
+					shoe.count -= 1
+				players[x].get(mybuffer)
+			mybuffer = shoe.spit()
+			if(mybuffer in [2,3,4,5,6,7]):
+				shoe.count += 1
+			if(mybuffer == 10):
+				shoe.count -= 2
+			if(mybuffer == "A"):
+				shoe.count -= 1
+			table.get(mybuffer)
 		
 
 		#Check if a player got blackjack 
 		for person in players: 
 			if person.eval() == 21: person.bank += person.bet + person.bet*2.5
 			person.bet = 0
+			person.done = True
 		
 		#Players turn to hit/stand
 		for person in players:
 			#Arbitary rule hitting while under 17
-			while(person.eval() < 17):
-				person.get(shoe.spit())
+			while(person.eval() < 12):
+				mybuffer = shoe.spit()
+				if(mybuffer in [2,3,4,5,6,7]):
+					shoe.count += 1
+				if(mybuffer == 10):
+					shoe.count -= 2
+				if(mybuffer == "A"):
+					shoe.count -= 1
+				person.get(mybuffer)
 				if(person.eval() > 21):
 					person.lost()
-					shoe.garbage.append(person.rem())
+					shoe.get(person.rem())
+			if(person.eval() > 11 and shoe.count > 12):
+				mybuffer = shoe.spit()
+				if(mybuffer in [2,3,4,5,6,7]):
+					shoe.count += 1
+				if(mybuffer == 10):
+					shoe.count -= 2
+				if(mybuffer == "A"):
+					shoe.count -= 1
+				person.get(mybuffer)
+				if(person.eval() > 21):
+					person.lost()
+					shoe.get(person.rem())
+
 		
 		#If dealer has 21, everyone automatically loses (for now)
 		if(table.eval() == 21):
@@ -148,9 +193,16 @@ def runtests(numplayers,shoesize,magicnumber,bankroll,lowbet,highbet):
 				person.lost()
 
 		#Dealer's turn to get cards
-		while(table.eval() < 18):
-			#Arbitrary hit at 17
-			table.get(shoe.spit())
+		while(table.eval() < 18 and "A" in table.hand):
+			#Arbitrary hit at soft 17 and lower
+			mybuffer = shoe.spit()
+			if(mybuffer in [2,3,4,5,6,7]):
+				shoe.count += 1
+			if(mybuffer == 10):
+				shoe.count -= 2
+			if(mybuffer == "A"):
+				shoe.count -= 1
+			table.get(mybuffer)
 		
 		#Check to see if dealer is over
 		if(table.eval() > 21):
@@ -169,19 +221,20 @@ def runtests(numplayers,shoesize,magicnumber,bankroll,lowbet,highbet):
 			else:
 				person.bank += person.bet
 
+
 		#print([players[x].eval() for x in range(numplayers)],table.eval())
-		#print([round(players[x].bank/float(bankroll),2) for x in range(numplayers)])
 		#print([players[x].hand for x in range(numplayers)])
 
 		#Takeback all cards and put it in garbage
 		for person in players:
-			shoe.garbage.append(person.rem())
-		
+			shoe.get(person.rem())
+			person.done = False
+	return players[0].bank
 
-		
-
-
-
-runtests(5,5,17,1000,5,100)
-
-
+last = 5
+results = []
+while last < 2500 and last > 0:
+	last = runtests(1,5,17,2500,5,25)
+	print last
+	results.append(last)
+print results
