17954a13f0be77dfbda10be4eb3d793439c3693b
LexWraith <lex.wraith@gmail.com>
2013-04-02 15:57:45 -0400

    Halfway to finishing transformation.

--- blackjackr2.py
+++ blackjackr2.py
@@ -1,4 +1,17 @@
 import numpy as np
+"""
+I decided to make the shoe NumPy and the hand NOT NumPy.
+It made sense to make the shoe NumPy, especially with larger decks.
+That logic didn't seem to matter as much for individual hands.
+I also just create a new deck when the shoe is up. Seems faster
+and more logical.
+
+Notes from last time:
+Dealer stands iff dealer is hard 17.
+Surrendering allows you to get half your bet back
+Implement a "stand" flag for players
+Blackjack gets you your money immediately.
+"""
 
 class shoe:
     def __init__(self,
@@ -7,14 +20,13 @@ class shoe:
                  decksets = 4
                      ):
         decksize = decksets * len(deck)
-        self.deck = np.array((decksize) * size)
+        self.deck = np.array(deck * decksize * size)
         """
         Yellow card is the mark at which the dealer will shuffle.
         The shoe's count variable is the counter towards yellow card,
         NOT the count used for betting progression.
         """
-        print(self.deck)
-        self.yellowcard = np.random.randint(self.deck/2, self.deck - 30)
+        self.yellowcard = np.random.randint(self.deck.size/2,self.deck.size-30)
         self.count = 0
         self.reset = False
         np.random.shuffle(self.deck)
@@ -24,10 +36,50 @@ class shoe:
 
     def deal(self):
         self.count += 1
-        if count > yellowcard : self.reset = True
+        if self.count > self.yellowcard : self.reset = True
         todeal = self.deck[0]
-        np.delete(self.deck,0)
+        self.deck = np.delete(self.deck,0)
+        return todeal
+
+class player:
+    def __init__(self,bank,lowbet = 25, hibet = 100):
+        self.hand = []
+        self.bank = bank
+        self.low = lowbet
+        self.high = hibet
+        self.done = False
+        self.bet = 0
+        self.betbig = False
+    
+    def evalhand(self):
+        if("A" not in self.hand):
+            return(sum(self.hand))
+        else:
+            while("A" in self.hand):
+                self.hand[self.hand.index("A")] = 1
+            while(sum(self.hand) > 21):
+                if(1 in self.hand):
+                    self.hand[self.hand.index(11)] = 1
+                else:
+                    break
+            return sum(self.hand)
+    
+    def get(self,card):
+        if(card == "A"):
+            self.hand.append("A")
+        else:
+            print card
+            self.hand.append(card)
+
+    def reset(self):
+        self.hand = []
+
+    def show(self):
+        print(self.hand)
 
 x = shoe(2)
-x.show()
+jamie = player(1000)
+while(not "A" in jamie.hand):
+    jamie.get(x.deal())
+jamie.show()
 
