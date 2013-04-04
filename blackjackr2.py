258d0350bee5b5d5e85f0713be9280cb38156a7e
LexWraith <lex.wraith@gmail.com>
2013-04-04 15:43:53 -0400

    Almost done with base.

--- blackjackr2.py
+++ blackjackr2.py
@@ -18,7 +18,7 @@ class shoe:
                  size,
                  deck = ["A",2,3,4,5,6,7,8,9,10,10,10,10],
                  decksets = 4
-                     ):
+                 ):
         decksize = decksets * len(deck)
         self.deck = np.array(deck * decksize * size)
         """
@@ -42,17 +42,30 @@ class shoe:
         self.deck = np.delete(self.deck,0)
         return todeal
 
+
 class player:
-    def __init__(self,bank,lowbet = 25, hibet = 100):
+    def __init__(self,
+                 bank,lowbet = 25, 
+                 hibet = 100,
+                 countsystem = {"2":1, "3":1, "4":1, "5":1, "6":1, 
+                                "7":0, "8":0, "9":0, "10":-1, "A":-1}
+                 ):
         self.hand = []
         self.bank = bank
         self.low = lowbet
         self.high = hibet
         self.done = False
         self.bet = 0
+        self.mycount = 0
         self.betbig = False
-    
+        self.countsystem = countsystem
+
     def evalhand(self):
+        """
+        This method might be a bit redundant,
+        but it allows adjustment of hands to optimal
+        value. It is useful post deal.
+        """
         if(not 11 in self.hand and not 1 in self.hand):
             return sum(self.hand)
         else:
@@ -77,13 +90,69 @@ class player:
     def show(self):
         print(self.hand)
 
-x = shoe(2)
-players = np.array([player(1000),player(1000)],dtype=object)
-players[:].get(x.deal())
-jamie = player(1000)
-for z in range(5):
-    jamie.get(x.deal())
-jamie.show()
-print(jamie.evalhand())
-jamie.show()
+    def count(self,card):
+        self.mycount += self.countsystem[card]
+
+class dealer:
+    def __init__(self):
+        self.hand = []
+        
+    def evalhand(self):
+        if(not 11 in self.hand and not 1 in self.hand):
+            return sum(self.hand)
+        else:
+            while(sum(self.hand) > 21):
+                if(11 in self.hand):
+                    self.hand[self.hand.index(11)] = 1
+                else:
+                    break
+            return sum(self.hand)
+        
+    def show(self):
+        print(self.hand)
+
+    def reset(self):
+        self.hand = []
+
+    def get(self,card):
+        if(card == "A" and sum(self.hand) <= 10):
+            self.hand.append(11)
+        elif(card == "A"):
+            self.hand.append(1)
+        else:
+            self.hand.append(int(card))
+
+#players = np.array([player(1000),player(1000)],dtype=object)
+#players[:].get(x.deal())
+
+def runtest(num_players,numdecks = 1,initial_bank = 1000):
+    tableshoe = shoe(numdecks)
+    """
+    Unsure if I want to make dealer part of the table.
+    table = np.array([dealer()])
+    table = np.append(table,[player(initial_bank) for z in range(num_players)])
+    """
+    tabledealer = dealer()
+    table = np.array([player(initial_bank) for z in range(num_players)])
+    for i in range(2):
+        for seats in table:
+            dealing = tableshoe.deal()
+            """
+            Giving each seat a chance to count the card.
+            """
+            for eseats in table:
+                eseats.count(dealing)
+            seats.get(tableshoe.deal())
+        tabledealer.get(tableshoe.deal())
+    
+    for seats in table:
+        print seats.hand
+        print seats.mycount
+
+    print tabledealer.hand
+
+    if(tableshoe.reset):
+        tableshoe = shoe(numdecks)
+
+runtest(5,1000)
 
