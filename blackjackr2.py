7db02c290f1046f3a5c6e5a65cc1b9f660cecce6
LexWraith <lex.wraith@gmail.com>
2013-04-08 09:55:22 -0400

    Almost done with engine.

--- blackjackr2.py
+++ blackjackr2.py
@@ -8,10 +8,11 @@ and more logical.
 
 Notes from last time:
 Dealer stands iff dealer is hard 17.
-Surrendering allows you to get half your bet back
-Implement a "stand" flag for players
-Blackjack gets you your money immediately.
+Blackjack gets you your money immediately, as opposed to pushing if dealer also blackjacks.
+The arbitrary truecount for these simulations will be +2.
+    If truecount >= +2, players will not hit.
 """
+
 
 class shoe:
     def __init__(self,
@@ -21,12 +22,9 @@ class shoe:
                  ):
         decksize = decksets * len(deck)
         self.deck = np.array(deck * decksize * size)
-        """
-        Yellow card is the mark at which the dealer will shuffle.
-        The shoe's count variable is the counter towards yellow card,
-        NOT the count used for betting progression.
-        """
+        #Marking when to use a new deck
         self.yellowcard = np.random.randint(self.deck.size/2,self.deck.size-30)
+        #Counter towards the yellowcard
         self.count = 0
         self.reset = False
         np.random.shuffle(self.deck)
@@ -45,10 +43,12 @@ class shoe:
 
 class player:
     def __init__(self,
-                 bank,lowbet = 25, 
+                 bank,
+                 lowbet = 25, 
                  hibet = 100,
                  countsystem = {"2":1, "3":1, "4":1, "5":1, "6":1, 
-                                "7":0, "8":0, "9":0, "10":-1, "A":-1}
+                                "7":0, "8":0, "9":0, "10":-1, "A":-1},
+                 countaccuracy = 100
                  ):
         self.hand = []
         self.bank = bank
@@ -59,10 +59,11 @@ class player:
         self.mycount = 0
         self.betbig = False
         self.countsystem = countsystem
+        self.countaccuracy = countaccuracy
 
     def evalhand(self):
         """
-        This method might be a bit redundant,
+        This method might be a bit redundant
         but it allows adjustment of hands to optimal
         value. It is useful post deal.
         """
@@ -91,7 +92,12 @@ class player:
         print(self.hand)
 
     def count(self,card):
-        self.mycount += self.countsystem[card]
+        """
+        Currently this only allows players to mis-read
+        cards, i.e. not factoring them into count.
+        """
+        if(np.random.randint(0,self.countaccuracy + 1)):
+            self.mycount += self.countsystem[card]
 
 class dealer:
     def __init__(self):
@@ -125,6 +131,17 @@ class dealer:
 #players = np.array([player(1000),player(1000)],dtype=object)
 #players[:].get(x.deal())
 
+
+
+def dealplayer(shoe,player,table):
+    """
+    This method handles dealing cards and counting.
+    """
+    dealing = shoe.deal()
+    for card_counters in table:
+        card_counters.count(dealing)
+    player.get(dealing)
+
 def runtest(num_players,numdecks = 1,initial_bank = 1000):
     tableshoe = shoe(numdecks)
     """
@@ -133,22 +150,67 @@ def runtest(num_players,numdecks = 1,initial_bank = 1000):
     table = np.append(table,[player(initial_bank) for z in range(num_players)])
     """
     tabledealer = dealer()
-    table = np.array([player(initial_bank) for z in range(num_players)])
-    for i in range(2):
-        for seats in table:
-            dealing = tableshoe.deal()
-            """
-            Giving each seat a chance to count the card.
-            """
-            for eseats in table:
-                eseats.count(dealing)
-            seats.get(tableshoe.deal())
-        tabledealer.get(tableshoe.deal())
+    table = np.array([player(initial_bank,countaccuracy = 50) for z in range(num_players)])
     
-    for seats in table:
-        print seats.hand
-        print seats.mycount
+    #Placing bets
+    """
+    Is there a point in making a wager memory slot?
+    For now I'm ignoring this.
+    """
 
+    #Initial deal
+    for i in range(2):
+        for seat in table:
+            dealplayer(tableshoe,seat,table)
+        dealplayer(tableshoe,tabledealer,table)
+    
+    #Check for blackjack
+    for seat in table:
+        if(seat.evalhand() == 21):
+            seat.bank += seat.high * 1.5 if seat.betbig else seat.low * 1.5
+            seat.done = True
+
+    #Player options
+    for seat in table:
+        if(seat.done): continue
+        while(seat.evalhand() <= 10):
+            dealplayer(tableshoe,seat,table)
+        #Arbitrary counting rule 1
+        if(seat.evalhand() <= 12 and seat.mycount/numdecks < -1):
+            dealplayer(tableshoe,seat,table)
+        #Arbitrary counting rule 2
+        if(seat.evalhand() <= 15 and seat.mycount/numdecks < -2):
+            dealplayer(tableshoe,seat,table)
+        seat.done = True
+
+
+        #At this point I am considering implementing basic strategy
+        #for the non-counting player
+
+    #Dealer checks for blackjack
+    if(tabledealer.evalhand() == 21):
+        pass
+
+    #Dealer options
+    while(tabledealer.evalhand() < 18):
+        dealplayer(tableshoe,tabledealer,table)
+
+    #Check if dealer busts
+    if(tabledealer.evalhand > 21):
+        for seat in table:
+            seat.bank += seat.high if seat.betbig else seat.low
+    else:
+    #Evaluate remaining vs dealer
+        for seat in table:
+            if (seat.evalhand() > 21) or (tabledealer.evalhand >= seat.evalhand()):
+                seat.bank -= seat.high if seat.betbig else seat.low
+            else:
+                seat.bank += seat.high if seat.betbig else seat.low
+    #Check if we need a new shoe
+
+    for seats in table:
+        print seats.hand,seats.bank,seats.mycount
+  
     print tabledealer.hand
 
     if(tableshoe.reset):
